cb863b7fc8d6d20d856e91d62c64dd3d69e649d9
Deborah Savage <savaged725@yahoo.com>
2019-07-12 15:49:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def using_push(array, string)
+  array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    array.push("violet")
+  end
+  
+  def using_unshift(array, string)
+    array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    array.unshift("Staten Island")
+  end
+  
+  def using_pop(array)
+    array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+    array.pop()
+  end
+  
+  def pop_with_args(array)
+   array = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+   array.pop(2)
+  end 
+    
+   def using_shift(array)
+    array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+    array.shift()
+  end
+  
+  def shift_with_args(array)
+     array = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    array.shift(2)
+  end
+  
+  def using_concat
+    
+  
+  
+  
+     
